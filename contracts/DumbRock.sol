a4567b99354c1d9f2393f59d9de666b7d91ae54d
jierlich <jierlich@umich.edu>
2021-12-25 18:48:20 -0500

    [FEAT] change fee model and base contract

--- contracts/DumbRock.sol
+++ contracts/DumbRock.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /**
     @title DumbRock
@@ -12,25 +12,32 @@ import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAut
     @notice It's just a dumb rock
 */
 
-contract DumbRock is Ownable, ERC721PresetMinterPauserAutoId {
+contract DumbRock is Ownable, ERC721 {
     address public wbtc;
 
-    uint256 public fee = 21000000 gwei;
+    // Ether is matic here since deployment is on Polygon
+    uint256 public fee = 4.2 ether;
+    /// @notice 0.0021 BTC
+    uint256 public btcCost = 210000; 
+    uint256 public count;
 
-    constructor(address _wbtc) ERC721PresetMinterPauserAutoId("DumbRock", "DUMB", "") {
+    constructor(address _wbtc) ERC721("DumbRock", "DUMB") {
         wbtc = _wbtc;
     }
 
     function mintRock(address _to) public payable {
         require(msg.value == fee, 'Incorrect fee amount');
-        IERC20(wbtc).transferFrom(msg.sender, address(this), 100000000);
-        super.mint(_to);
+        require(count < 2100, 'Max mint reached');
+        IERC20(wbtc).transferFrom(msg.sender, address(this), btcCost);
+        _safeMint(_to, count);
+        count += 1;
+        
     }
 
     function burnRock(uint256 _tokenId) public {
         require(super.ownerOf(_tokenId) == msg.sender, 'Only owner can burn');
-        super.burn(_tokenId);
-        IERC20(wbtc).transfer(msg.sender, 100000000);
+        _burn(_tokenId);
+        IERC20(wbtc).transfer(msg.sender, btcCost);
     }
 
     function withdraw() onlyOwner() public {
