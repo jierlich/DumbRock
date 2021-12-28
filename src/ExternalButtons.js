e06f0e2c662d1918cbde1e7498b2f5b86f403e15
jierlich <jierlich@umich.edu>
2021-12-28 13:30:51 -0500

    [FEAT] add external links

--- src/ExternalButtons.js
+++ src/ExternalButtons.js
@@ -1,10 +1,13 @@
 export default function ExternalButtons() {
   return (
     <div className="buttonContainer">
-      <form action="https://google.com" target="_blank">
+      <form
+        action="https://polygonscan.com/address/0xe850dD8b9D90F060BCA3A6C7f865228cF6C80062"
+        target="_blank"
+      >
         <button className="externalButton">polygonscan</button>
       </form>
-      <form action="https://google.com" target="_blank">
+      <form action="https://opensea.io/collection/dumbrock-v2" target="_blank">
         <button className="externalButton">opensea</button>
       </form>
     </div>
